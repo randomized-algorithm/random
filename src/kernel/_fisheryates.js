0dd55b3f1915d368936b3eed276db2be360f8bb1
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-04-06 18:36:38 +0200

    :books: docs(_fisheryates): Add important note about the definition of k.

--- src/kernel/_fisheryates.js
+++ src/kernel/_fisheryates.js
@@ -4,7 +4,9 @@
 
 const _fisheryates = (randint) => (n, a, i, j) => {
 	// We will swap at most n elements
-
+	// NOTE: When n = j - i, the last swap swaps a[j-1] with itself,
+	// which is a NOOP. /!\ HOWEVER, the last swap is NOT a NOOP when
+	// n < j - i. Hence we cannot let k = i + n - 1 in general.
 	const k = i + n;
 
 	for (; i < k; ++i) {
