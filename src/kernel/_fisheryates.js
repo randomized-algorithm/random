21c8bdd09fccb513adef8bf9b653d818e792ac85
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-04-06 18:50:59 +0200

    :books: docs(_fisheryates): Add references.

--- src/kernel/_fisheryates.js
+++ src/kernel/_fisheryates.js
@@ -1,7 +1,24 @@
 /**
- * Sample array using Fisher-Yates method.
+ * Sample element from an array using Fisher-Yates method.
+ *
+ * NOTE: The original description of the algorithm by Fisher and Yates [1] had
+ * unnecessary bookkeeping which made the algorithm run in O(n * (j-i)) time.
+ * This implementation follows Durstenfeld's [2] and Knuth's [3] descriptions
+ * which yield O(n) running time.
+ *
+ * For more information see the excellent "Fisher–Yates shuffle" page on
+ * Wikipedia [4]. Fisher and Yates description is referred there as
+ * "Fisher and Yate's original method" or "pencil-and-paper method".
+ * The more efficient implementation described by Durstenfeld and Knuth is
+ * referred there as "the modern method".
+ *
+ * [1] Fisher, Ronald A.; Yates, Frank (1938). Statistical tables for
+ *     biological, agricultural and medical research.
+ * [2] Durstenfeld, R. (July 1964). "Algorithm 235: Random permutation"
+ * [3] Knuth, Donald E. (1969). Seminumerical algorithms. The Art of Computer
+ *     Programming Volume 2.
+ * [4] https://en.wikipedia.org/wiki/Fisher–Yates_shuffle
  */
-
 const _fisheryates = (randint) => (n, a, i, j) => {
 	// We will swap at most n elements
 	// NOTE: When n = j - i, the last swap swaps a[j-1] with itself,
